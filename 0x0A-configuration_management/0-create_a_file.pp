0afc4409297fd698731e3424e7fd55f108aadc80
paschalugwu <ugwupaschal@gmail.com>
2024-02-27 00:39:17 +0100

    feat(puppet): Implement Puppet manifest to create file in /tmp
    Created a Puppet manifest () to efficiently create a file in the /tmp directory on an Ubuntu 20.04 LTS system. The manifest ensures the file has specific permissions, ownership, and content as required. Additionally, the manifest passes puppet-lint version 2.1.1 without any errors and runs without any issues.
    
    Footer:
    © 2024 ALX. All rights reserved.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file in /tmp
+
+file {'/tmp/school':
+    ensure  => 'present',
+    content => 'I love Puppet',
+    group   => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+}
