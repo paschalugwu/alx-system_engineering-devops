98defc8f18950c0809c2e72468d7600dada69562
paschalugwu <ugwupaschal@gmail.com>
2024-03-20 00:47:17 +0100

    chore(database-setup): Add script for MySQL installation and replication setup
    Added a bash script to automate the installation of MySQL 5.7 and set up a database and user for replication. The script updates package lists, installs MySQL server version 5.7, and performs initial configuration steps for replication.

--- 0x14-mysql/task0downgrade.sh
+++ 0x14-mysql/task0downgrade.sh
@@ -19,6 +19,15 @@ sudo rm -rf /var/lib/mysql/
 # Update the package lists for upgrades and new package installations
 sudo apt-get update -y
 
+# Download the MySQL APT repository config package
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.17-1_all.deb
+
+# Install the MySQL APT repository config package
+sudo dpkg -i mysql-apt-config_0.8.17-1_all.deb
+
+# Update the package lists for upgrades and new package installations
+sudo apt-get update -y
+
 # Install MySQL 5.7
 sudo apt-get install -y mysql-server-5.7
 
