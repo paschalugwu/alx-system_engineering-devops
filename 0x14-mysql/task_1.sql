e5593cc352f397f35a1149799905a82b6c8740b1
paschalugwu <ugwupaschal@gmail.com>
2024-03-20 08:23:27 +0100

    feat(database): Add replication user for project correction
    Added a new MySQL user 'holberton_user' with appropriate permissions for replication client access. This user will be utilized for project correction purposes.

---
+++ 0x14-mysql/task_1.sql
@@ -0,0 +1,5 @@
+mysql -u root -p <<EOF
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+EOF
