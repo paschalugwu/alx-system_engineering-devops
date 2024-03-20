c2b1d4ebe630df99e530c1825ba0f4f1c9c75258
paschalugwu <ugwupaschal@gmail.com>
2024-03-20 01:45:19 +0100

    chore(database-setup): Add script for MySQL installation and replication setup
    Added a bash script to automate the installation of MySQL 5.7 and set up a database and user for replication. The script updates package lists, installs MySQL server version 5.7, and performs initial configuration steps for replication.

--- 0x14-mysql/task_0.sh
+++ 0x14-mysql/task_0.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # This script installs MySQL and sets up a database and user for replication
 
-# Update the package lists for upgrades and new package installations
-sudo apt-get update -y
-
-# Install MySQL 5.7
-sudo apt-get install -y mysql-server-5.7.25
+sudo apt-key add signature.key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
+sudo sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
+sudo apt-get update
+sudo apt-cache policy mysql-server
+sudo apt install -f -y mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
