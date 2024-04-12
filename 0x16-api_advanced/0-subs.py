661edba73c3e21a43b97c5a73c8dfced5470c4ad
paschalugwu <ugwupaschal@gmail.com>
2024-04-12 05:07:48 +0100

    feat(task0): Implement number_of_subscribers function
    Implement the number_of_subscribers function in the task 0 module.
    The function queries the Reddit API to retrieve the number of subscribers for a given subreddit.
    It handles HTTP requests and parses JSON responses to extract subscriber counts.
    Ensure proper error handling for invalid subreddits.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-'''task 0 module'''
+''' task 0 module'''
+
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    '''Gets number of subs of the subreddit'''
+    '''gets num of subs of a subreddit'''
     headers = {'User-agent': 'test'}
     subs = requests.get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), allow_redirects=False, headers=headers)
+        sys.argv[1]), allow_redirects=False, headers=headers)
+
     if subs.status_code == 200:
-        return subs.json()['data']['subscribers']
+        return (subs.json()['data']['subscribers'])
     else:
         return 0
